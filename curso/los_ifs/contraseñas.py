44dc8f225cdd74fce5547ee7b61c237fc1961a74
Francesco Petrelli <fpetrelli@AGNAUM.LOCAL>
2026-05-29 13:01:05 -0300

    ultimo de funciones

--- curso/los_ifs/contraseñas.py
+++
@@ -1,9 +0,0 @@
-user = 'admin'
-password = 'yo123'
-
-ingresar_user = input('ingrese el numbre de usuario: ')
-ingresar_password = input('ingrese la clave: ')
-
-comprobar =  (user == ingresar_user.strip() and password == ingresar_password.strip())
-
-print (f'las credenciales son correctas: {comprobar}')
--- curso/los_ifs/rango_valores.py
+++
@@ -1,8 +0,0 @@
-minimo = 0
-maximo = 5
-
-valor = int(input('ingresar el valor: '))
-
-comprobar = (minimo <= valor and maximo >= valor)
-
-print (f'el valor se encuentra en rango: {comprobar}')
